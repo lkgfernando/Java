807d91b5a8368051bab55eb0e530873c4ff9de0a
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-09-14 11:29:37 -0300

    Update da classe Calculator

--- MembrosEstaticos/src/application/Program.java
+++ MembrosEstaticos/src/application/Program.java
@@ -10,18 +10,18 @@ public class Program {
     public static void main(String[] args){
         Locale.setDefault(Locale.ENGLISH);
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator();
+
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
+        double c = Calculator.circumference(radius);
 
-        double v = calc.volume(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n ", c);
         System.out.printf("Volume: %.2f%n ", v);
-        System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
 
 
 
